3e156f931cbff9b68a38cf0790e735c3f8aa4900
WMD2077 <201722832@qq.com>
2021-08-06 15:53:01 +0800

    update

---
+++ .history/test-site/scripts/main_20210806155258.js
@@ -0,0 +1,28 @@
+let image = document.querySelector('img');
+
+image.onclick = function() {
+    let src = image.getAttribute('src');
+    if(src === 'images/firefox.png') image.setAttribute('src', 'images/firefox2.png');
+    else image.setAttribute('src', 'images/firefox.png');
+}
+
+let button = document.querySelector('button');
+let heading = document.querySelector('h1');
+
+function setUserName() {
+    myName = prompt('请输入你的名字');
+    if(!myName || myName === null) setUserName();
+    localStorage.setItem('name', myName);
+    heading.textContent = '你好， ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let myName = localStorage.getItem('name');
+    heading.textContent = '你好， ' + myName;
+}
+
+button.onclick = function() {
+    setUserName();
+}
--- gen.sh
+++
@@ -1,1 +0,0 @@
-cp .\test-site\*  .\docs\ -r
